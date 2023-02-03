e25f3bd42c6c6e0daf0aea7c36567dc66ca25dfd
nicolassoam <nsm4456@gmail.com>
2023-02-03 00:57:03 -0300

    initial structure

--- src/server.js
+++ src/server.js
@@ -1,19 +1,7 @@
-import express from 'express'
-import cors from "cors";
-
-const app = express();
-
-app.use(express.json());
-app.use(cors({
-  origin: "*"
-}));
+import app from './routes.js';
+const port = process.env.PORT || 3333;
 
 
-app.get('/', (req, res) => {
-  console.log("GET /");
-  res.status(200).send("Hello World");
-});
-
-app.listen('3333', () => {
-  console.log("O servidor foi inicializado em http://localhost:3333");
+app.listen(port, () => {
+  console.log(`O servidor foi inicializado em http://localhost:${port}`);
 });
